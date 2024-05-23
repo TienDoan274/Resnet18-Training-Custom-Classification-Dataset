5ff1029611526f867938eeae96a511b5c5ea7101
TienDoan274 <doannhattien4@gmail.com>
2024-05-23 10:04:16 +0700

    a

--- Resnet18_torchvisionModels/predict.py
+++ Resnet18_torchvisionModels/predict.py
@@ -4,24 +4,49 @@ from torch import nn
 from torchvision.transforms import ToTensor,Compose,Normalize,Resize
 from PIL import Image
 import os
+from dataset import CustomData
+from torch.utils.data import DataLoader
+
+import argparse
+def get_args():
+    parser = argparse.ArgumentParser(description='build_and_train_resnet18')
+    parser.add_argument('--data_path',type=str,default="test_images")
+    parser.add_argument('--output_path',type=str,default='')
+    parser.add_argument('--checkpoint_path','-ckp',type=str,default='saved_models/best.pt')
+
+    args = parser.parse_args()
+    return args
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-print(device)
-model = resnet18(weights = ResNet18_Weights.DEFAULT)
-ckp = torch.load('./saved_models/best.pt')
-num_features = model.fc.in_features
-model.fc = nn.Linear(num_features , 5)
-model.load_state_dict(ckp['model'])
-model.to(device)
-model.eval()
-transform = Compose([
-                ToTensor(), 
-                Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-                ])
-img_path = os.path.join('././data/test/Truck/003490_17.jpg')
-image = Image.open(img_path).convert('RGB')
-image = transform(image)[None,:,:,:].to(device)
-pred = model(image)
+def main(args):
 
-print(torch.max(pred,dim = 1).indices)
+    print(device)
+    model = resnet18(weights = ResNet18_Weights.DEFAULT)
+    # ckp = torch.load(args.checkpoint_path)
+    num_features = model.fc.in_features
+    model.fc = nn.Linear(num_features , 5)
+    # model.load_state_dict(ckp['model'])
+    model.to(device)
+    model.eval()
+    transform = Compose([
+                    Resize((224,224)), 
+                    ToTensor(), 
+                    Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+                    ])
+    output = os.path.join(args.output_path,'output.txt')
+    with open(output, 'w') as f:
+        for img_path in os.listdir(args.data_path):
+            image_path = os.path.join(args.data_path, img_path)
+            if os.path.isfile(image_path):  
+                image = Image.open(image_path).convert('RGB')
+                image = transform(image)
+                image = image.to(device)
+                model.eval()
+                with torch.no_grad():
+                    pred = model(image.unsqueeze(0))  # Add batch dimension
+                prediction = int(torch.max(pred, dim=1).indices)
+                f.writelines(f"{prediction}\n")
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
